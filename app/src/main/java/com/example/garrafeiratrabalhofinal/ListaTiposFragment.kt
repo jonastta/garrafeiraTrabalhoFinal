4a9cfcb5b9809f55a81fcc7c0f51d8a9ea8aec92
Jonas <jonas789@outlook.com>
2023-05-26 10:04:55 +0100

    Adicionar interface LoaderManager.LoaderCallbacks<Cursor> ao fragmento para implementar no layout dos tipos

--- app/src/main/java/com/example/garrafeiratrabalhofinal/ListaTiposFragment.kt
+++ app/src/main/java/com/example/garrafeiratrabalhofinal/ListaTiposFragment.kt
@@ -1,20 +1,22 @@
 package com.example.garrafeiratrabalhofinal
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.Loader
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.garrafeiratrabalhofinal.databinding.FragmentListaTiposBinding
 
 
-private var _binding: FragmentListaTiposBinding? = null
-// This property is only valid between onCreateView and
-// onDestroyView.
-private val binding get() = _binding!!
+class ListaTiposFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
-class ListaTiposFragment : Fragment() {
+    private var _binding: FragmentListaTiposBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,5 +42,17 @@ class ListaTiposFragment : Fragment() {
     }
 
     companion object {
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        TODO("Not yet implemented")
     }
 }
