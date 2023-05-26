0cbf6274abb010e9c5102bbfb09fc38d4dd1c10e
Jonas <jonas789@outlook.com>
2023-05-26 09:19:05 +0100

    Criado o adaptador de tipos(faz o interface com o rcycleview)

---
+++ app/src/main/java/com/example/garrafeiratrabalhofinal/AdapterTipos.kt
@@ -0,0 +1,24 @@
+package com.example.garrafeiratrabalhofinal
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class AdapterTipos : RecyclerView.Adapter<AdapterTipos.ViewHolderTipos>() {
+    inner class ViewHolderTipos(itemView: View) :ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderTipos {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderTipos, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
